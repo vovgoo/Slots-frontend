3cbcfd5a8c6390901ac7d4477b2aa03a3c0baf51
vovgoo <vov.gonchar2017@yandex.ru>
2024-04-30 02:24:41 +0300

    Change animations

--- script.js
+++ script.js
@@ -31,8 +31,36 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	progressTabs.forEach((tab, index) => {
     if(index < 3){
-      tab.style.animationDelay = `${index * 0.5}s`;
+      tab.style.animationDelay = `${index * 0.3}s`;
       tab.classList.add("yellow");
     }
 	})
 })
+
+function animateMoney(target, start, end, duration) {
+	const element = document.getElementById(target);
+    const range = end - start;
+    const increment = range > 0 ? 1 : -1;
+    const stepValue = Math.abs(Math.ceil(range / duration)); 
+    let current = start;
+
+    const timer = setInterval(function() {
+        current += increment * stepValue;
+        element.innerHTML = current.toLocaleString();
+
+        if ((increment === 1 && current >= end) || (increment === -1 && current <= end)) {
+            clearInterval(timer);
+            element.innerHTML = end.toLocaleString();
+        }
+    }, 10); 
+
+    if (duration < 10) {
+        element.innerHTML = end.toLocaleString();
+    }
+}
+
+
+
+window.onload = function() {
+	animateMoney("moneyCounter", 0, 10300205, 100);
+}
